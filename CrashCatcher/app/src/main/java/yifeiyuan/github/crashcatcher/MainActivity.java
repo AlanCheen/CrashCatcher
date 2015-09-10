c50af6fd655169cf840410dc4b943c591c2fea7a
AlanCheen <2985697630@qq.com>
2015-09-10 15:49:39 +0800

    add no-op library

--- CrashCatcher/app/src/main/java/yifeiyuan/github/crashcatcher/MainActivity.java
+++ CrashCatcher/app/src/main/java/yifeiyuan/github/crashcatcher/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.tv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                throw new IllegalArgumentException("test crash catcher");
+            }
+        });
     }
 
     @Override
